c86b76a65db62b2237f1eb5b79e513f00f0bbc78
emBrileg08 <46973724+emBrileg08@users.noreply.github.com>
2019-02-26 12:39:34 -0500

    Updated from Brython Server: 2/26/2019 12:39:34 PM

--- cryptography.py
+++ cryptography.py
@@ -11,37 +11,40 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 
-choice=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+choice="space filler"
 
-if choice=="q":
-    print("Goodbye!")
-elif choice=="e":
-    message=list(input("Message: "))
-    key=list(input("Key: "))
-    length=len(message)
-    while len(key)<length:
-        key.extend(key[::])
-    for x in range(0,length):
-        num=associations.find(message[x])
-        num2=associations.find(key[x])
-        num3=num+num2
-        if (num3/84)<1:
+while choice!= "q":
+    choice=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if choice=="q":
+        print("Goodbye!")
+    elif choice=="e":
+        message=list(input("Message: "))
+        key=list(input("Key: "))
+        length=len(message)
+        while len(key)<length:
+            key.extend(key[::])
+        for x in range(0,length):
+            num=associations.find(message[x])
+            num2=associations.find(key[x])
+            num3=num+num2
+            if (num3/84)<1:
+                print(associations[num3],end="")
+            elif (num3%84)==0:
+                print(associations[84],end="")
+            else:
+                remain=num3%84
+                print(associations[remain-1],end="")
+        print("")
+    elif choice=="d":
+        message=list(input("Message: "))
+        key=list(input("Key: "))
+        length=len(message)
+        while len(key)<length:
+            key.extend(key[::])
+        for x in range(0,length):
+            num=associations.find(message[x])
+            num2=associations.find(key[x])
+            num3=num-num2
             print(associations[num3],end="")
-        elif (num3%84)==0:
-            print(associations[84],end="")
-        else:
-            remain=num3%84
-            print(associations[remain-1],end="")
-elif choice=="d":
-    message=list(input("Message: "))
-    key=list(input("Key: "))
-    length=len(message)
-    while len(key)<length:
-        key.extend(key[::])
-    for x in range(0,length):
-        num=associations.find(message[x])
-        num2=associations.find(key[x])
-        num3=num-num2
-        print(associations[num3],end="")
-else:
-    print("Did not understand command, try again.")
+    else:
+        print("Did not understand command, try again.")
